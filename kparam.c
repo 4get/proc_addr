856919332aee266af71ce30fbb529e34a5d48cce
yangyaowu <iyaowu@gmail.com>
2011-11-29 11:33:52 +0800

    ok, use extern symble and modify it.

--- kparam.c
+++ kparam.c
@@ -6,8 +6,23 @@
 
 MODULE_LICENSE("Dual BSD/GPL");
 
+extern int pte_print_enable;
+extern int pte_print_pid;
+int pe;
+int pid;
+
+module_param(pe, int, S_IRUGO);
+module_param(pid, int, S_IRUGO);
+
+//EXPORT_SYMBOL(pte_print_enable);
+//EXPORT_SYMBOL(pte_print_pid);
+
 static int kparam_init(void)
 {   
+	printk("old pte_print_enable=%d,pte_print_pid=%d.\n",pte_print_enable,pte_print_pid);
+	pte_print_enable=pe;
+	pte_print_pid=pid;
+	printk("new pte_print_enable=%d,pte_print_pid=%d.\n",pte_print_enable,pte_print_pid);
 	return 0;
 }   
 
